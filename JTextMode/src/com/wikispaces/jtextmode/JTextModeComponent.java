2ff48a49abb75922f914dbfaede1bfb1b34fa045
William <William@192.168.1.106>
2012-08-13 15:09:29 -0400

    Has initial public API, and basically works.

--- JTextMode/src/com/wikispaces/jtextmode/JTextModeComponent.java
+++ JTextMode/src/com/wikispaces/jtextmode/JTextModeComponent.java
@@ -9,13 +9,15 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.FilteredImageSource;
 import java.awt.image.ImageFilter;
 import java.awt.image.ImageProducer;
 import java.awt.image.RGBImageFilter;
 import java.io.IOException;
-import java.util.Random;
+import java.util.LinkedList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
@@ -24,20 +26,67 @@ import javax.swing.JComponent;
  * 
  *
  */
+@SuppressWarnings("serial")
 public class JTextModeComponent extends JComponent {
-	public int rows;
-	public int columns;
+	/**
+	 * The size of the text screen in characters.
+	 */
+	private int rows;
+	private int columns;
 
-	public int cursorX;
-	public int cursorY;
-	public boolean cursorVisible;
+	/**
+	 * The position of the cursor on the text screen.
+	 */
+	private int cursorX;
+	private int cursorY;
 
-	public int[][] displayMemory;
+	/**
+	 * Whether the cursor is rendered
+	 */
+	private boolean cursorVisible;
+
+	/**
+	 * The characters rendered on the screen.
+	 */
+	public int[][] charsDisplayed;
+
+	/**
+	 * The colors that the characters are rendered in.
+	 */
 	public byte[][] foregroundColor;
+
+	/**
+	 * Note that on a real DOS display, EITHER 8-15 would be valid background
+	 * colors OR blinking could be enabled.
+	 */
 	public byte[][] backgroundColor;
+
+	/**
+	 * The blinking bit for each character on the display.
+	 */
 	public boolean[][] blinking;
 
-	// For drawing, in each foreground color
+	/**
+	 * If true, the cursor and blinking characters will be rendered on or
+	 * invisible as the timer has it. Otherwise, such as when you redraw the
+	 * screen occasionally, turn this off to have blinking characters always
+	 * visible.
+	 * */
+	private boolean allBlinkingEnabled;
+
+	// The metadata for characters drawn by the cursor
+	private byte currDrawColor = 15;
+	private byte currDrawBGColor = 0;
+	private boolean currDrawBlinking = false;
+
+	/**
+	 * These are images of the characters to be drawn on the screen. Their
+	 * required dimensions: CHARACTER: 9 px wide, 16 px high. IMAGE: 256
+	 * characters arranged to be 32 wide by 8 high. More than 256 characters to
+	 * still be arranged in 32 wide rows.<br>
+	 * <br>
+	 * One image is required in this array for each color text will be drawn in.
+	 */
 	private static Image codePage[];
 
 	private static final int[][] colors = { { 0, 0, 0 }, { 0, 0, 127 },
@@ -46,8 +95,9 @@ public class JTextModeComponent extends JComponent {
 			{ 0, 255, 0 }, { 0, 255, 255 }, { 255, 0, 0 }, { 255, 0, 255 },
 			{ 255, 255, 0 }, { 255, 255, 255 } };
 
-	public JTextModeComponent(int rows, int columns)
-			throws IllegalArgumentException, MissingCodePageException {
+	public JTextModeComponent(int rows, int columns, boolean blinkingEnabled,
+			boolean showCursor) throws IllegalArgumentException,
+			MissingCodePageException {
 		if (rows <= 0 || columns <= 0) {
 			throw new IllegalArgumentException(
 					"Rows and columns must be greater than 0");
@@ -95,21 +145,50 @@ public class JTextModeComponent extends JComponent {
 
 		cursorX = 0;
 		cursorY = 0;
-		cursorVisible = false;
+		cursorVisible = showCursor;
 
-		displayMemory = new int[columns][rows];
+		charsDisplayed = new int[columns][rows];
 		foregroundColor = new byte[columns][rows];
 		backgroundColor = new byte[columns][rows];
 		blinking = new boolean[columns][rows];
 
-		Random r = new Random();
 		for (int x = 0; x < columns; x++) {
 			for (int y = 0; y < rows; y++) {
-				displayMemory[x][y] = r.nextInt(256);
-				foregroundColor[x][y] = (byte) r.nextInt(16);
-				backgroundColor[x][y] = (byte) r.nextInt(16);
+				charsDisplayed[x][y] = 32;
+				foregroundColor[x][y] = 15;
+				backgroundColor[x][y] = 0;
 				blinking[x][y] = false;
 			}
+		}
+
+		if (blinkingEnabled) {
+			allBlinkingEnabled = true;
+			Thread blinkThread = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					boolean lastRunState = false;
+					while (true) {
+						boolean currentState = false;
+						if (System.currentTimeMillis() % 1000 < 500) {
+							currentState = true;
+						}
+						if (currentState != lastRunState) {
+							repaint();
+							lastRunState = currentState;
+						}
+						try {
+							Thread.sleep(100);
+						} catch (InterruptedException e) {
+							break;
+						}
+					}
+				}
+
+			});
+			blinkThread.start();
+		} else {
+			allBlinkingEnabled = false;
 		}
 	}
 
@@ -122,23 +201,52 @@ public class JTextModeComponent extends JComponent {
 				BufferedImage.TYPE_INT_ARGB);
 		Graphics2D frameGraphics = frameBuffer.createGraphics();
 		RenderingHints renderingHints = new RenderingHints(
-	             RenderingHints.KEY_TEXT_ANTIALIASING,
-	             RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		renderingHints.put(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-	    frameGraphics.setRenderingHints(renderingHints);
+				RenderingHints.KEY_INTERPOLATION,
+				RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+		frameGraphics.setRenderingHints(renderingHints);
+
+		boolean blinkOnThisFrame = false;
+		// Decide, if blinking, whether to draw or blank blinking characters
+		if (allBlinkingEnabled && (System.currentTimeMillis() % 1000 < 500)) {
+			blinkOnThisFrame = true;
+		}
 
 		// Place each individual character images into the frame
 		for (int x = 0; x < columns; x++) {
 			for (int y = 0; y < rows; y++) {
-				int pageSheetColumn = displayMemory[x][y] % 32;
-				int pageSheetRow = displayMemory[x][y] / 32;
-				frameGraphics.drawImage(codePage[foregroundColor[x][y]], x * 9,
-						y * 16, (x + 1) * 9, (y + 1) * 16, pageSheetColumn * 9,
-						pageSheetRow * 16, (pageSheetColumn + 1) * 9,
-						(pageSheetRow + 1) * 16, new Color(
-								colors[backgroundColor[x][y]][0],
-								colors[backgroundColor[x][y]][1],
-								colors[backgroundColor[x][y]][2]), null);
+				if (allBlinkingEnabled && blinking[x][y] && blinkOnThisFrame) {
+					// Draw background color only
+					frameGraphics.setColor(new Color(
+							colors[backgroundColor[x][y]][0],
+							colors[backgroundColor[x][y]][1],
+							colors[backgroundColor[x][y]][2]));
+					frameGraphics.fillRect(x * 9, y * 16, 9, 16);
+				} else {
+					int charToDraw = charsDisplayed[x][y];
+
+					// Handle the cursor
+					if (cursorVisible && x == cursorX && y == cursorY) {
+						if (blinkOnThisFrame) {
+							// Cursor is a space
+							charToDraw = 32;
+						} else {
+							// Cursor is an underscore
+							charToDraw = 95;
+						}
+					}
+
+					// Draw character
+					int pageSheetColumn = charToDraw % 32;
+					int pageSheetRow = charToDraw / 32;
+					frameGraphics.drawImage(codePage[foregroundColor[x][y]],
+							x * 9, y * 16, (x + 1) * 9, (y + 1) * 16,
+							pageSheetColumn * 9, pageSheetRow * 16,
+							(pageSheetColumn + 1) * 9, (pageSheetRow + 1) * 16,
+							new Color(colors[backgroundColor[x][y]][0],
+									colors[backgroundColor[x][y]][1],
+									colors[backgroundColor[x][y]][2]), null);
+
+				}
 			}
 		}
 
@@ -151,6 +259,9 @@ public class JTextModeComponent extends JComponent {
 	}
 
 	/**
+	 * Takes an image and replaces the given color with the color
+	 * "completely transparent."
+	 * 
 	 * Source: http://stackoverflow.com/questions/665406/how-to-make-a-color-
 	 * transparent-in-a-bufferedimage-and-save-as-png
 	 * 
@@ -158,7 +269,8 @@ public class JTextModeComponent extends JComponent {
 	 * @param color
 	 * @return
 	 */
-	public static Image makeColorTransparent(BufferedImage im, final Color color) {
+	private static Image makeColorTransparent(BufferedImage im,
+			final Color color) {
 		ImageFilter filter = new RGBImageFilter() {
 
 			// the color we are looking for... Alpha bits are set to opaque
@@ -177,5 +289,161 @@ public class JTextModeComponent extends JComponent {
 
 		ImageProducer ip = new FilteredImageSource(im.getSource(), filter);
 		return Toolkit.getDefaultToolkit().createImage(ip);
+	}
+
+	/**
+	 * @param text
+	 */
+	public void writeLn(String... text) {
+		for (String currString : text) {
+			char[] textChars = currString.toCharArray();
+			for (int i = 0; i < textChars.length; i++) {
+				putAtCursor(textChars[i]);
+			}
+			cursorMoveNewline();
+		}
+
+		if (text.length <= 0) {
+			cursorMoveNewline();
+		}
+	}
+
+	/**
+	 * 
+	 * @param chars
+	 */
+	public void writeLn(char... chars) {
+		for (char currChar : chars) {
+			putAtCursor(currChar);
+			cursorMoveNewline();
+		}
+
+		if (chars.length <= 0) {
+			cursorMoveNewline();
+		}
+	}
+
+	public void writeLn() {
+		cursorMoveNewline();
+	}
+
+	public void write(String... text) {
+		for (String currString : text) {
+			char[] textChars = currString.toCharArray();
+			for (int i = 0; i < textChars.length; i++) {
+				putAtCursor(textChars[i]);
+			}
+		}
+	}
+
+	public void write(char... chars) {
+		for (char currChar : chars) {
+			putAtCursor(currChar);
+		}
+	}
+	
+	public void clearScreen () {
+		scrollScreen(rows);
+	}
+
+	private boolean doneReading = false;
+	public String readLn() {
+		doneReading = false;
+		final StringBuilder readBuffer = new StringBuilder();
+		addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				readBuffer.append(e.getKeyChar());
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					doneReading = true;
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+		});
+		while (!doneReading) {
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+				break;
+			}
+		}
+		return readBuffer.toString();
+	}
+
+	public void setDrawColor(byte color) {
+		currDrawColor = color;
+	}
+
+	public void setDrawBGColor(byte bgColor) {
+		currDrawBGColor = bgColor;
+	}
+
+	public void setDrawBlinking(boolean blinking) {
+		currDrawBlinking = blinking;
+	}
+
+	private void putAtCursor(int character) {
+		charsDisplayed[cursorX][cursorY] = character;
+		foregroundColor[cursorX][cursorY] = currDrawColor;
+		backgroundColor[cursorX][cursorY] = currDrawBGColor;
+		blinking[cursorX][cursorY] = currDrawBlinking;
+		incrementCursor();
+	}
+
+	private void incrementCursor() {
+		cursorX++;
+		if (cursorX >= columns) {
+			cursorMoveNewline();
+		}
+	}
+
+	private void cursorMoveNewline() {
+		cursorX = 0;
+		cursorY++;
+		if (cursorY >= rows) {
+			scrollScreen(1);
+		}
+	}
+
+	private void scrollScreen(int scrollRows) {
+		// Shift one row at a time
+		for (int i = 0; i < scrollRows; i++) {
+			// Shift every row on the screen up
+			for (int y = 1; y < rows; y++) {
+				for (int x = 0; x < columns; x++) {
+					charsDisplayed[x][y - 1] = charsDisplayed[x][y];
+					foregroundColor[x][y - 1] = foregroundColor[x][y];
+					backgroundColor[x][y - 1] = backgroundColor[x][y];
+					blinking[x][y - 1] = blinking[x][y];
+				}
+			}
+
+			// Clear bottom row
+			for (int x = 0; x < columns; x++) {
+				charsDisplayed[x][rows - 1] = 32;
+				foregroundColor[x][rows - 1] = currDrawColor;
+				backgroundColor[x][rows - 1] = currDrawBGColor;
+				blinking[x][rows - 1] = currDrawBlinking;
+			}
+
+			// Move cursor up
+			cursorY--;
+			if (cursorY < 0) {
+				cursorY = 0;
+			}
+		}
 	}
 }
--- JTextMode/src/com/wikispaces/jtextmode/JTextModeDemo.java
+++ JTextMode/src/com/wikispaces/jtextmode/JTextModeDemo.java
@@ -2,9 +2,12 @@ package com.wikispaces.jtextmode;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.SwingWorker;
 import javax.swing.border.EmptyBorder;
 
 public class JTextModeDemo extends JFrame {
@@ -29,24 +32,33 @@ public class JTextModeDemo extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * @throws MissingCodePageException 
+	 * @throws IllegalArgumentException 
 	 */
-	public JTextModeDemo() {
+	public JTextModeDemo() throws IllegalArgumentException, MissingCodePageException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 495, 344);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
+
+		final JTextModeComponent c = new JTextModeComponent(5, 20, true, true);
+		c.requestFocusInWindow();
+
+		contentPane.add(c);
+
 		try {
-			contentPane.add(new JTextModeComponent (25, 80));
-		} catch (IllegalArgumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (MissingCodePageException e) {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		c.writeLn("Hello world! I am a very long string of text that should wrap around the edge of the screen.");
+		c.writeLn();
+		c.writeLn("And scroll.");
+		c.write("Yes or no? ");
 	}
 
 }
